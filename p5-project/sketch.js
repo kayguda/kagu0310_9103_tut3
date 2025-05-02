217f1f99cd9c1382e50ec3d4223e97333627ad5d
kayguda <kagu0310@uni.sydney.edu.au>
2025-05-02 15:25:29 +1000

    1
    1

--- p5-project/sketch.js
+++ p5-project/sketch.js
@@ -1,7 +1,82 @@
+let img;
+let numSegments = 50;
+let segments = [];
+let drawSegments = false;
+let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
+let canvasAspectRatio = 0;
+
+function preload() {
+  img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  imgDrwPrps.aspect = img.width / img.height;
+  canvasAspectRatio = width / height;
+  calculateImageDrawProps();
+  let segmentWidth = img.width / numSegments;
+  let segmentHeight = img.height / numSegments;
+  
+
+  for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
+    for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
+      let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
+       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
+       segments.push(segment);
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
+  if (drawSegments) {
+    for (const segment of segments) {
+      segment.draw();
+    }
+  } else {
+    image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
+  }
+}
+function keyPressed() {
+  if (key == " ") {
+    drawSegments = !drawSegments;
+  }
+}
+
+
+function calculateImageDrawProps() {
+
+  if (imgDrwPrps.aspect > canvasAspectRatio) {
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = width / imgDrwPrps.aspect;
+    imgDrwPrps.yOffset = (height - imgDrwPrps.height) / 2;
+    imgDrwPrps.xOffset = 0;
+  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
+    imgDrwPrps.height = height;
+    imgDrwPrps.width = height * imgDrwPrps.aspect;
+    imgDrwPrps.xOffset = (width - imgDrwPrps.width) / 2;
+    imgDrwPrps.yOffset = 0;
+  }
+  else if (imgDrwPrps.aspect == canvasAspectRatio) {
+    imgDrwPrps.width = width;
+    imgDrwPrps.height = height;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = 0;
+  }
+}
+class ImageSegment {
+
+  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
+    this.srcImgSegXPos = srcImgSegXPosInPrm;
+    this.srcImgSegYPos = srcImgSegYPosInPrm;
+    this.srcImgSegWidth = srcImgSegWidthInPrm;
+    this.srcImgSegHeight = srcImgSegHeightInPrm;
+    this.srcImgSegColour = srcImgSegColourInPrm;
+  }
+
+  draw() {
+    stroke(0);
+    fill(this.srcImgSegColour);
+    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
+  }
 }
